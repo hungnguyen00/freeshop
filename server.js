c8bc28a5fe871ddb35cabfb2f26267f8b07c8538
hungnguyen <hung.nguyen@zinio.com>
2017-09-24 01:10:48 +0700

    Add static admin html template

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs') // template engine is ejs
+app.use(express.static('public')); // css, js, image,....
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
+app.get('/admin', function (req, res) {
+   res.render('admin/template/index');
 })
+app.get('/admin_template', function (req, res) {
+   res.render('admin/template/index_template');
+})
+
+
 
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Running")
 })
